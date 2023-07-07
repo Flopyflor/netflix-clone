102470569b4f9c9036abf8a38968b88e351d6a82
Florencia Allami <allami.florencia@gmail.com>
2023-07-06 21:55:48 -0300

    only FE

--- netflix-clone-BE/index.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js'
-import connectToDatabase from './config/db.js';
-
-const port = app.get('port')
-
-app.listen(port, async () => {
-    await connectToDatabase();
-    console.log('funcionando en el puerto', port)
-});
